1291f8112bf9f17d2779c5eacd6c7f14a012d2fb
Kody McNamara <Kody McNamara@10.7.13.49>
2024-02-20 18:08:57 -0600

    Fixed placement error of toString in car

--- src/hw4/Car.java
+++ src/hw4/Car.java
@@ -152,7 +152,6 @@ public class Car extends Vehicle{
 		public void startEngine() {
 			
 		}
-}
 		//PUSHSTART
 
 @Override
@@ -160,4 +159,5 @@ public String toString() {
 	return "Car [brand=" + brand + ", make=" + make + ", modelYear=" + modelYear + ", price=" + price + ", color="
 			+ color + ", fuelType=" + fuelType + ", mileage=" + mileage + ", mass=" + mass + ", cylinders="
 			+ cylinders + ", gasTankCapacity=" + gasTankCapacity + ", startType=" + startType + "]";
+	}
 }
