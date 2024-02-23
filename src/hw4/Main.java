14b935c2fa89751a61f0680fb202d268dfc69b59
venitiab <venitia@localhost>
2024-02-22 19:01:16 -0600

    updated main method to test functionality of methods

--- src/hw4/Main.java
+++ src/hw4/Main.java
@@ -10,13 +10,30 @@ public class Main {
 		vehicleManager.initializeStock();
 		
 		// Display all vehicle information.
+		System.out.println("All Vehicle Information:");
+		System.out.println("------------------------");
 		vehicleManager.displayAllVehicleInformation();
+		System.out.println();
 		
 		// Display all car information.
+		System.out.println("All Car Information:");
+		System.out.println("------------------------");
 		vehicleManager.displayAllCarInformation();
+		System.out.println();
 		
 		// Display all motorbike information.
+		System.out.println("All MotorBike Information:");
+		System.out.println("------------------------");
 		vehicleManager.displayAllMotorBikeInformation();
+		System.out.println();
+		
+		//test to see if highest/lowest maintenance is working
+		System.out.println("------------------------");
+		Vehicle highMaintVehicle = vehicleManager.getVehicleWithHighestMaintenanceCost(300);
+		System.out.println("Vehicle with Highest Maintenance Cost:" + highMaintVehicle);
+		
+		Vehicle lowMaintVehicle = vehicleManager.getVehicleWithLowestMaintenanceCost(300);
+		System.out.println("Vehicle with Lowest Maintenance Cost:" + lowMaintVehicle);
 		}
 
 	}
